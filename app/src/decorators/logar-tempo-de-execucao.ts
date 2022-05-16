350c5b8eb9b3c99fa19531cba449220da0b23141
Caitano <josiasmartinscaitano@gmail.com>
2022-05-16 07:27:15 -0300

    feat: api decorators

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,4 +1,10 @@
 // decorators: é uma funão
 export function logarTempoDeExecucao() {
-    
+    return function(
+        target: any,
+        propertyKey: string,
+        descriptor: PropertyDescriptor
+    ) {
+        return descriptor;
+    }
 }
