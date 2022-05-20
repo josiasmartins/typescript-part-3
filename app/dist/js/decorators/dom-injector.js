e94a0fa6072f0216f88ebc4fa14177198688d597
Caitano <josiasmartinscaitano@gmail.com>
2022-05-20 09:31:31 -0300

    feat: problema de excesso de busca no dom

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -1,9 +1,12 @@
 export function domInjector(seletor) {
     return function (target, propertyKey) {
+        console.log(`Modificando prototype ${target.constructor.name} e adicionado o getter para a propriedade ${propertyKey}`);
         console.log(propertyKey);
         const getter = function () {
             const elemento = document.querySelector(seletor);
+            console.log(`buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`);
             return elemento;
         };
+        Object.defineProperty(target, propertyKey, { get: getter });
     };
 }
--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -2,11 +2,19 @@ export function domInjector(seletor: string) {
     // target: se vc colocar na propriedade estática de uma class, retorna uma função contructora
     // se não for: ele retorna o prototype dessa classe;
     return function(target: any, propertyKey: string) {
+        console.log(`Modificando prototype ${target.constructor.name} e adicionado o getter para a propriedade ${propertyKey}`)
         console.log(propertyKey);
 
         const getter = function() {
             const elemento = document.querySelector(seletor);
+            console.log(`buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`)
             return elemento;
         }
+
+        Object.defineProperty(
+            target, 
+            propertyKey, 
+            { get: getter }
+        )
     }
 }
