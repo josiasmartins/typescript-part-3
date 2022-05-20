fcb06345f156c4cba666f98ee1a183b5e005f566
Caitano <josiasmartinscaitano@gmail.com>
2022-05-20 08:13:09 -0300

    feat: adiconado escape decorators

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,20 +1,16 @@
-import { inspect } from '../controllers/inspect.js';
+import { inspect } from '../decorators/inspect.js';
 import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
-    private escapar = false;
 
-    constructor(seletor: string, escapar?: boolean) {
+    constructor(seletor: string) {
         const elemento = document.querySelector(seletor);
         if (elemento) {
             this.elemento = elemento as HTMLElement;
         } else {
             throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
-        }
-        if (escapar) {
-            this.escapar = escapar;
         }
     }
 
@@ -25,10 +21,6 @@ export abstract class View<T> {
         // const t1 = performance.now();
 
         let template = this.template(model);
-        if (this.escapar) {
-            template = template
-                .replace(/<script>[\s\S]*?<\/script>/, '');
-        }
         this.elemento.innerHTML = template;
         // const t2 = performance.now();
         // console.log(`Tempo de execução do método update: ${(t2 - t1)/1000} segundos`);
