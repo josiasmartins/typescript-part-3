04432c05a9185bbe14c215163513b9026d54b0ee
Caitano <josiasmartinscaitano@gmail.com>
2022-05-20 09:15:28 -0300

    feat: criado getter dinamicamente

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -1,4 +1,9 @@
 export function domInjector(seletor) {
     return function (target, propertyKey) {
+        console.log(propertyKey);
+        const getter = function () {
+            const elemento = document.querySelector(seletor);
+            return elemento;
+        };
     };
 }
--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -2,6 +2,11 @@ export function domInjector(seletor: string) {
     // target: se vc colocar na propriedade estática de uma class, retorna uma função contructora
     // se não for: ele retorna o prototype dessa classe;
     return function(target: any, propertyKey: string) {
+        console.log(propertyKey);
 
+        const getter = function() {
+            const elemento = document.querySelector(seletor);
+            return elemento;
+        }
     }
 }
